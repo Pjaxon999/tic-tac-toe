e254ab3de20946da06ffde7297982da015897263
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-02-19 10:41:47 -0700

    adjust styling, add script skeleton
    I've spent a lot of time thinking about how I want this whole thing to
    be structured which is why it is taking so long, but I think I now have
    a basic outline that I can work with for the script which will come to
    define the project. The question is whether or not I can write it in a
    way that works and makes sense. Wish me luck!

--- scripts.js
+++ scripts.js
@@ -1,2 +1,34 @@
 // Test external link
 console.log("Here I am! Tic me like a Tac Toe!");
+
+// Store and update gameboard state
+function gameboard() {
+    const boardSpace = [];
+
+};
+
+// Logic for when a round is played
+function moveMade() {
+
+};
+
+// Change the player after a valid move
+function playerToggle() {
+
+};
+
+// Make sure nobody has won yet, if they have, let everybody know!
+function winChecker() {
+
+};
+
+
+// Rendering function
+function render() {
+
+};
+
+// Connect the UI to the Game State
+// insert eventListener here
+
+// Render the game when the page loads here!
