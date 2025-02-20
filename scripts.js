feeaba584250461ac5c0595b9a84a193d7a88286
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-02-20 10:58:30 -0700

    Adjust cell styling, add drawing logic
    I have basically given up on writing this code but I am pushing through
    because I enjoy torture. Basically I have no idea what I am doing and
    have no clue how to actually write this utilizing factory funtions or
    IIFEs so instead I am going to try and get some of the game working
    using methods that I would normally use that are apparently now wrong.
    Then I guess the idea is to take my garbage code and then refactor it
    into something that actually utilizes the methods that TOP wants me to
    utilize but didn't teach me properly. Somehow.

--- scripts.js
+++ scripts.js
@@ -1,42 +1,70 @@
-// Test external link
-console.log("Here I am! Tic me like a Tac Toe!");
+// // Test external link
+// console.log("Here I am! Tic me like a Tac Toe!");
 
-// Store and update gameboard state. Should be able to retrieve board state,
-// add an X or O, and read the board state out to the console
-function gameState() {
-    // store the "board", should start as empty
-    const boardSpace = ['', '', '', '', '', '', '', '', ''];
-
-
-};
-
-// Logic for when a round is played
-function moveMade() {
-    if (gameOver = true){
-        return
-    }
-
-};
-
-// Change the player after a valid move
-function playerToggle() {
-
-};
-
-// Make sure nobody has won yet, if they have, let everybody know!
-function winChecker() {
-
-    // after someone wins or a draw is declared, the game should probably end.
-    gameOver = true;
-};
+// // Store and update gameboard state. Should be able to retrieve board state,
+// // add an X or O, and read the board state out to the console
+// function gameState() {
+//     // store the "board", should start as empty
+//     const boardSpace = ['', '', '', '', '', '', '', '', ''];
 
 
-// Rendering function
-function render() {
+// };
 
-};
+// // Logic for when a round is played
+// function moveMade() {
+//     if (gameOver = true){
+//         return
+//     }
+
+// };
+
+// // Change the player after a valid move
+// function playerToggle() {
+
+// };
+
+// // Make sure nobody has won yet, if they have, let everybody know!
+// function winChecker() {
+
+//     // after someone wins or a draw is declared, the game should probably end.
+//     gameOver = true;
+// };
+
+
+// // Rendering function
+// function render() {
+
+// };
 
 // Connect the UI to the Game State
 // insert eventListener here
 
 // Render the game when the page loads here!
+
+
+// since I have no fucking clue what I am doing I am just going to pretend all this shit doesn't exist and try to code it how I would normally do it and then try to refactor it after the fact lol yay I am so happy
+
+const board = document.querySelectorAll(".cell");
+const gameBoard = ['', '', '', '', '', '', '', '', ''];
+let gameOver = "false";
+let player = "X";
+
+board.forEach((cell) => {
+    cell.addEventListener("click", makeMark)
+});
+
+function makeMark(e){
+    if (player === "X" && gameOver === "false" && e.target.textContent === '') {
+        e.target.textContent = "X";
+        let boardIndex = e.target.getAttribute("data-index");
+        gameBoard.splice(boardIndex, 1, "X");
+        player = "O";
+    } else if (player === "O" && gameOver === "false" && e.target.textContent === '') {
+        e.target.textContent = "O";
+        let boardIndex = e.target.getAttribute("data-index");
+        gameBoard.splice(boardIndex, 1, "O");
+        player = "X";
+    } else {
+        return
+    }
+};
