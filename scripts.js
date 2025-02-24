9d7e8c289503d589396e7798d9c54983117010cd
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-02-24 11:50:27 -0700

    Separate game/board state, update divs to buttons
    The game and board state sections of the script are now handled
    separately and there is a new section which, after I have finished
    figuring out how to check for a winner, will render the current state
    of the board to the UI. I also switched from using clickable divs to
    using clickable buttons for accessibility in the HTML file.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,50 @@
-const board = ['', '', '', '', '', '', '', '', ''];
-let gameOver = false;
-let playerTurn = "X";
-function makeMove(space){
-    if (gameOver === false && playerTurn === "X" && board[space] === ''){
-        board.splice(space, 1, "X");
-    } else if (gameOver === false && playerTurn === "O" && board[space] === ''){
-        board.splice(space, 1, "O");
-    } else {
-        return
-    }
-    playerTurn = playerTurn === "X" ? "O" : "X";
+function boardState(){
+    let board = ['', '', '', '', '', '', '', '', ''];
+
+    const reset = () => {
+        board = ['', '', '', '', '', '', '', '', ''];
+        console.log("The game has been reset");
+    };
+
+    const getBoard = () => board;
+
+    return {reset, getBoard};
 };
+
+function gameState(){
+    const board = boardState();
+    const playerTurn = "X";
+    const getPlayer = () => playerTurn;
+
+    // Loop through each combination in winningCombinations.
+    // For each combination, check if all three indices in the board array have the same value (X or O).
+    // If any combination meets this condition, return true (a winner exists).
+    // Check to see if anyone has won or if there is a draw
+    const winningCombinations = [
+        [0,1,2],[3,4,5],[6,7,8], // rows
+        [0,3,6],[1,4,7],[2,5,8], // verts
+        [0,4,8],[2,4,6] // diags
+    ];
+    
+    function gameEndCheck(){
+        
+    }
+
+    const makeMove = (space) => {
+        if (gameOver === true || board[space] !== '' || space < 0 || space >= board.length){
+            return
+        } else {
+            board[space] = playerTurn;
+            console.log(board);
+            gameEndCheck();
+            playerTurn = playerTurn === "X" ? "O" : "X";
+        }
+    };
+
+    return {getPlayer};
+}
+
+function render(){
+    // render relevant game state and board state information to the UI
+}
+
