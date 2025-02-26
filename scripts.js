be6bc53bcf12cd6b5a763006d9938326424632b1
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-02-26 10:36:30 -0700

    Add logic for gameEndCheck script, adjust h2 class
    So I have a (hopefully) working version of the logic I need to check if
    the move that the current player just made is a game ending move (win or
    draw). This works by first generating an array that contains the indexes
    of where the player's respective marks are on the board ("X" marks for
    the X player, "O" marks for the O player). Using this array, I check to
    see if the pattern of indexes matches any of the patterns in the
    winningCombinations array. If there is a match, then we have a winner!
    if there is no match, we then need to check if the game is still going
    on or not. I have implemented this by simply checking to see if there
    are any spaces left on the board. If there are no spaces, then it must
    be a draw. If there are still spaces left, there is no winner, and the
    game can continue. All I need to do now is figure out how to implement
    it properly.
    
    I also added a class to the H2 in anticipation of having to target it
    in my render function to update the UI when the player's turn changes.
    
    I also added an object that contains the player's names and their
    respective markers. I might update the code to use the markers instead
    of just passing the playerTurn variable, but I am again anticipating
    that I will need to have the players names stored somewhere to render
    to the UI in order to allow players to input their own custom names.

--- scripts.js
+++ scripts.js
@@ -6,15 +6,29 @@ function boardState(){
         console.log("The game has been reset");
     };
 
+    // Allow the board to be updated from elsewhere
     const getBoard = () => board;
 
     return {reset, getBoard};
 };
 
 function gameState(){
+    // Store the board state, initialize variables to 
+    // track the game state
     const board = boardState();
     let playerTurn = "X";
+    let player1 = {
+        name : "",
+        mark : "X",
+        winCount: 0
+    }
+    let player2 = {
+        name: "",
+        mark: "O",
+        winCount: 0
+    }
     let gameOver = false;
+    
 
     const getPlayer = () => playerTurn;
 
@@ -25,18 +39,47 @@ function gameState(){
         [0,3,6],[1,4,7],[2,5,8], // verts
         [0,4,8],[2,4,6] // diags
     ];
+
     
-    function gameEndCheck(){
+    // Return an array of the indexes of "X" marks if it is 
+    // X's turn, or "O" marks if it is O's turn
+    let indexToCheck = [];
+    function indexReturner (array){
         
+        for (i = 0; i < array.length; i++) {
+            if (playerTurn === "X" && array[i] === "X") {
+                indexToCheck.push(i);
+            } else if (playerTurn === "O" && array[i] ==="O"){
+                indexToCheck.push(i);
+            }
+        }
     }
 
+    function gameEndCheck(array, combos) {
+        for (const combo of combos) {
+            if (combo.every(index => array.includes(index))) {
+                declareGameOver();            
+                return true;
+            }
+         }
+        if (array.includes("")){
+            return false;
+        } else {
+            declareGameOver();
+        }
+    }
+
+    // If a move is valid and the game is still going on, make the move, update the board,
+    // and check to see if it was a game ending move (win or draw). If the game is still going,
+    // it is now the next players turn.
     const makeMove = (space) => {
         if (gameOver === true || board.getBoard()[space] !== '' || space < 0 || space >= board.getBoard().length){
             return
         } else {
             board.getBoard()[space] = playerTurn;
-            console.log(board);
-            gameEndCheck();
+            console.log(board.getBoard());
+            indexReturner(board.getBoard());
+            gameEndCheck(indexToCheck, winningCombinations);
         }
         changePlayer();
     };
